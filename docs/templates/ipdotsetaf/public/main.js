f67e896eff286ffcd4c8a9e86ace9d281032d5c2
IPdotSetAF <ipdotsetaf@gmail.com>
2025-09-15 00:25:33 +0330

    added github and website links in header

---
+++ docs/templates/ipdotsetaf/public/main.js
@@ -0,0 +1,14 @@
+export default {
+    iconLinks: [
+        {
+          icon: 'github',
+          href: 'https://github.com/IPdotSetAF/TgBotPlay.WebAPI',
+          title: 'GitHub'
+        },
+        {
+          icon: 'globe',
+          href: 'https://ipdotsetaf.ir',
+          title: 'IPdotSetAF'
+        }
+      ]
+}
